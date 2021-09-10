ab2e5e60befd5d741669760d22ec065bfcdd516a
Roberto Ubieto Juan <robertouj@gmail.com>
2021-09-10 12:06:29 +0200

    implement tests for the authorization class methods

--- challenges/backend/src/app/services/CarOnSaleClient/classes/test/CarOnSaleClient.spec.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/test/CarOnSaleClient.spec.ts
@@ -3,11 +3,16 @@ import { CarOnSaleClient } from "../CarOnSaleClient";
 import { ICarOnSaleClient } from "../../interface/ICarOnSaleClient";
 import { DependencyIdentifier } from "../../../../DependencyIdentifiers";
 import { Container } from "inversify";
+import { IAuth } from "../../../Auth/interface/IAuth";
+import { HardcodedAuth } from "../../../Auth/classes/HardcodedAuth";
+import { ILogger } from "../../../Logger/interface/ILogger";
+import { Logger } from "../../../Logger/classes/Logger";
 
 // define related test blocks
 describe('CarOnSaleClient tests using ASSERT inteface from CHAI module: ', () => {
 
   let carOnSaleClient: ICarOnSaleClient;
+  let hardcodedAuth: IAuth;
 
   beforeEach(() => {
     /*
@@ -20,13 +25,15 @@ describe('CarOnSaleClient tests using ASSERT inteface from CHAI module: ', () =>
     /*
      * Register dependencies in DI environment.
      */
+    container.bind<ILogger>(DependencyIdentifier.LOGGER).to(Logger);
     container.bind<ICarOnSaleClient>(DependencyIdentifier.CARONSALECLIENT).to(CarOnSaleClient);
+    container.bind<IAuth>(DependencyIdentifier.AUTH).to(HardcodedAuth);
 
     /*
      * Inject all dependencies in the application & retrieve application instance.
      */
     carOnSaleClient = container.get<ICarOnSaleClient>(DependencyIdentifier.CARONSALECLIENT);
-    
+    hardcodedAuth = container.get<IAuth>(DependencyIdentifier.AUTH);
 
   });
 
@@ -36,6 +43,26 @@ describe('CarOnSaleClient tests using ASSERT inteface from CHAI module: ', () =>
       let result;
       result = await carOnSaleClient.getRunningAuctions();
       assert.typeOf(result, 'array');
+    });
+  });
+
+  describe('Check Functions from HardcodedAuth: ', () => {
+    describe('Check getAuthToken() Function from HardcodedAuth: ', () => {
+
+      it('Check string returned value using: assert.notEqual;', async () => {
+        let result;
+        result = await hardcodedAuth.getAuthToken();
+        assert.typeOf(result, 'string');
+      });
+    });
+
+    describe('Check isValidToken() Function from HardcodedAuth: ', () => {
+
+      it('Check boolean returned value using: assert.notEqual;', async () => {
+        let result;
+        result = await hardcodedAuth.isValidToken("");
+        assert.typeOf(result, 'boolean');
+      });
     });
   });
 });
