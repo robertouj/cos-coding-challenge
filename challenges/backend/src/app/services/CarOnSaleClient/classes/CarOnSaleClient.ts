e69d4ebe2600c2768f4b93acbb4da627d57a5627
Roberto Ubieto Juan <robertouj@gmail.com>
2021-09-10 12:15:54 +0200

    implement functionality of getRunningAuctions()

--- challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
@@ -1,16 +1,35 @@
 import { ICarOnSaleClient } from "../interface/ICarOnSaleClient";
 import { IAuction } from "../interface/IAuction";
-import { injectable } from "inversify";
-
+import { inject, injectable } from "inversify";
+import { DependencyIdentifier } from "../../../DependencyIdentifiers";
+import { ILogger } from "../../../services/Logger/interface/ILogger";
+import { IAuth } from "../../../services/Auth/interface/IAuth";
+import { ConfigGlobals } from "../../../ConfigGlobals";
+import axios from "axios";
 import "reflect-metadata";
 
 @injectable()
 export class CarOnSaleClient implements ICarOnSaleClient {
 
-    public constructor() {
+    public constructor(
+        @inject(DependencyIdentifier.LOGGER) private logger: ILogger,
+        @inject(DependencyIdentifier.AUTH) private auth: IAuth) {
     }
 
     public async getRunningAuctions(): Promise<Array<IAuction>> {
-        return [];
+        try {
+            const result = await axios
+                .get(`${ConfigGlobals.API_COS}${ConfigGlobals.API_COS_GET_AUCTIONS}`, {
+                    headers: {
+                        userid: this.auth.getUserId(),
+                        authtoken: await this.auth.getAuthToken()
+                    }
+                });
+            return result.data.items;
+        } catch (error) {
+            this.logger.log(<string>error);
+            process.exit(-1);
+            return [];
+        }
     }
 }
