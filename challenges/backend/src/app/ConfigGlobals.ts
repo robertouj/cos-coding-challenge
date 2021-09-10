8654b1c4c219664025314171f276fd455a27ae9e
Roberto Ubieto Juan <robertouj@gmail.com>
2021-09-10 11:29:42 +0200

    create configuration file for the service

---
+++ challenges/backend/src/app/ConfigGlobals.ts
@@ -0,0 +1,10 @@
+/**
+ * Config file of the service
+ */
+export const ConfigGlobals = {
+    API_COS: "https://api-core-dev.caronsale.de/api",
+    API_COS_GET_AUCTIONS: "/v2/auction/buyer/",
+    API_COS_AUTH: "/v1/authentication/",
+    HARCODED_USER: "salesman@random.com",
+    HARDCODED_PASS: "123test"
+};
